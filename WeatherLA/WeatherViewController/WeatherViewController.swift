f506f72dfffc6415bd018675c445b402df75d634
DanielyanVK <danielyanvk@gmail.com>
2021-12-23 16:06:28 +0300

    added few comments for future reference

--- WeatherLA/WeatherViewController/WeatherViewController.swift
+++ WeatherLA/WeatherViewController/WeatherViewController.swift
@@ -11,7 +11,7 @@ import Alamofire
 import SwiftyJSON
 
 class WeatherViewController: UIViewController {
-    
+// Outlets for all labels and ImageView
     @IBOutlet weak var locationLabel: UILabel!
     @IBOutlet weak var conditionImageView: UIImageView!
     @IBOutlet weak var dateLabel: UILabel!
@@ -25,29 +25,36 @@ class WeatherViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Do any additional setup after loading the view.
+        // Calling our functions
         requestWeather()
         requestDay()
     }
-    // Define request
+    // Defing request to get weather
     private func requestWeather() {
         AF
             .request("https://api.openweathermap.org/data/2.5/weather?id=5368361&appid=d7a6b9add4b8e5439475eeb1317cec30&units=metric")
             .responseJSON {
                 response in
                 if let weatherResponse = try? response.result.get() {
+                    // Stores data in JSON format
                     let jsonResponse = JSON(weatherResponse)
+                    // We get access to nested folder
                     let jsonWeather = jsonResponse["weather"].array![0]
                     let jsonTemp =  jsonResponse["main"]
+                    // Converting to non JSON values
                     let jsonIconName = jsonWeather["icon"].stringValue
                     let jsonLocationName = jsonResponse["name"].stringValue
                     let jsonCondition = jsonWeather["main"].stringValue
+                    // Accesing nested "folder" by typing ["main folder"][sub folder]
                     let jsonWind = jsonResponse["wind"]["speed"].stringValue
                     let jsonHum = jsonTemp["humidity"].stringValue
                     
+                    // Assigning values to image and labels
                     self.locationLabel.text = jsonLocationName
+                    // We use UIImage(named: "string") so we can assign the image we need depending on current condition
                     self.conditionImageView.image = UIImage(named: jsonIconName)
                     self.conditionLabel.text = jsonCondition
+                    // This way we can show our value in non-decimal format
                     self.temperatureLabel.text = "\(Int(round(jsonTemp["temp"].doubleValue)))"
                     self.windSpeedLabel.text = "\(jsonWind) km/h"
                     self.humidityLabel.text = "\(jsonHum) %"
@@ -58,8 +65,10 @@ class WeatherViewController: UIViewController {
     func requestDay() {
         let date = Date()
         let formatter = DateFormatter()
+        // This format shows only day of the week
         formatter.dateFormat = "EEEE"
         let result = formatter.string(from: date)
+        // Assigning current day of the week to the label
         dateLabel.text! = result
     }
 }
